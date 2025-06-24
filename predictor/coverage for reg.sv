9cfceac46fd244eaaecee36d43aae5ddecf32920
Altamash Shahid <altamashshahid93@gmail.com>
2025-06-24 15:17:39 +0530

    Update coverage for reg.sv

--- predictor/coverage for reg.sv
+++ predictor/coverage for reg.sv
@@ -568,5 +568,12 @@ module tb;
 
   
 endmodule
-
+//////////////////////////////
+//rundo file
+vsim +access+r;
+run -all;
+acdb save;
+acdb report -db  fcover.acdb -txt -o cov.txt -verbose  
+exec cat cov.txt;
+exit
 
